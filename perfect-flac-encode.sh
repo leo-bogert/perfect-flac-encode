6a7f6690cdaf8a897dff7e4b6fc0d0f64d504906
leo-bogert <github@leo.bogert.de>
2012-11-28 18:17:47 +0100

    Use TEMP_DIRS_ABSOLUTE

--- perfect-flac-encode.sh
+++ perfect-flac-encode.sh
@@ -632,8 +632,8 @@ pretag_singletrack_flacs_from_cue_or_die()
 
 test_flac_singletracks_or_die() {
 	echo "Running flac --test on singletrack FLACs..."
-	local inputdir_flac="$INPUT_DIR_ABSOLUTE/${TEMP_DIRS[FLAC_SINGLETRACK_SUBDIR]}"
-	local flac_files=( "$inputdir_flac"/*.flac )
+	
+	local flac_files=( "${TEMP_DIRS_ABSOLUTE[FLAC_SINGLETRACK_SUBDIR]}"/*.flac )
 	
 	if ! flac --test --silent --warnings-as-errors "${flac_files[@]}"; then
 		echo "Testing FLAC singletracks failed!" >&2
