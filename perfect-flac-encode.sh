3e25a7719174191852bd788684c0538256eee97d
leo-bogert <github@leo.bogert.de>
2012-12-23 03:03:18 +0100

    Add important FIXMEs to get rid of command substitution misuse. I have learned that the output of $() is subject to globbing. This is dangerous since we use $() as part of file name generation!

--- perfect-flac-encode.sh
+++ perfect-flac-encode.sh
@@ -29,6 +29,8 @@ set -o pipefail	# this is absolutely critical: make pipes exit with failure if a
 ################################################################################
 # TODO: replace all occurences of "echo <failure message> >&2 ; exit 1" with a function call
 # TODO FIXME XXX: I have been misusing bash command substitution, i.e. $() and ``. I had assumed that "exit" inside of the substituted function would kill the outer script, but it will only exit the subshell which executes the command substituion! => Review all $() and `` and check $? for sucess/failure of the called function/program where necessary.
+# TODO FIXME XXX: All uses of `` command substituion should be replaced with $() because it is more robust.
+# TODO FIXME XXX: All uses of command substituion should be put within double quotes. I had assumed that command subtitution prevents globbing but it does not! This is very dangerous because we use the output of command subtitution for generating filenames in some places!
 ################################################################################
 
 ################################################################################
